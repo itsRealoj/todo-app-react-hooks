eab497f24529030e90b0fc515d33a7def521971e
Stephen Oj <stephenojwang1040@gmail.com>
2020-06-18 07:47:41 +0300

    create todo list static page

--- src/App.js
+++ src/App.js
@@ -3,24 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				TODO APPLICATION
+				<div>
+					<ul>
+						<li>first todo</li>
+						<li>second todo</li>
+						<li>third todo</li>
+					</ul>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
