3a6dac6bd18087e5d7bfe18920c82083a62a4687
Stephen Oj <stephenojwang1040@gmail.com>
2020-06-18 08:17:06 +0300

    created a provider for the components

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -22,3 +22,14 @@ const reducer = (state, action) => {
 			throw new Error();
 	}
 };
+
+//create a provider for components to consume and subscribe to changes
+export const TodoContextProvider = (props) => {
+	const [state, dispatch] = useReducer(reducer, initState);
+
+	return (
+		<TodoContext.Provider value={[state, dispatch]}>
+			{props.children}
+		</TodoContext.Provider>
+	);
+};
