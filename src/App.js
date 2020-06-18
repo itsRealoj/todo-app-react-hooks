cfe048ab309a6c3465448ba9b0e2c0b02c36424b
Stephen Oj <stephenojwang1040@gmail.com>
2020-06-18 08:52:58 +0300

    wrapped the app with a context provider

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TodoContext, TodoContextProvider } from './contexts/TodoContext';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				TODO APPLICATION
-				<div>
-					<ul>
-						<li>first todo</li>
-						<li>second todo</li>
-						<li>third todo</li>
-					</ul>
-				</div>
-			</header>
+			<TodoContextProvider>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					TODO APPLICATION
+					<div>
+						<ul>
+							<li>first todo</li>
+							<li>second todo</li>
+							<li>third todo</li>
+						</ul>
+					</div>
+				</header>
+			</TodoContextProvider>
 		</div>
 	);
 }
